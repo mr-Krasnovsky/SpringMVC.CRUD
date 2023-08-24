04e1cf19d3ea9370e18e540993fd944fbba21293
Maxim Krasnov <mr.krasnovsky@gmail.com>
2023-08-24 13:19:30 +0300

    Automatic generation of the ID value Auto-increment, Serial

--- SpringMVC.CRUD.JDBC.Template/src/main/java/ru/krasnov/springcourse/dao/PersonDAO.java
+++ SpringMVC.CRUD.JDBC.Template/src/main/java/ru/krasnov/springcourse/dao/PersonDAO.java
@@ -33,7 +33,7 @@ public class PersonDAO {
     }
 
     public void save(Person person) {
-	jdbcTemplate.update("INSERT INTO Person VALUES(1, ?, ?, ?)", person.getName(), person.getAge(),
+	jdbcTemplate.update("INSERT INTO Person(name, age, email) VALUES(?, ?, ?)", person.getName(), person.getAge(),
 		person.getEmail());
     }
 
@@ -93,7 +93,7 @@ public class PersonDAO {
     public List<Person> create1000People() {
 	List<Person> people = new ArrayList<>();
 	for (int i = 0; i < 1000; i++) {
-	    people.add(new Person(1, "Name" + i, 30, "test" + i + "@mail.com"));
+	    people.add(new Person(i, "Name" + i, 30, "test" + i + "@mail.com"));
 	}
 	return people;
     }
